cd55d3c706e89ae36cd0e05806f444e687b3e841
podestal <luisrodriguezpodesta@gmail.com>
2023-12-18 05:05:38 -0500

    fetching ap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import React from "react";
+
+const KEY = "3f5dd940"
+
 function App() {
+
+  const [movies, setMovies] = React.useState([])
+  const [watched, setWatched] = React.useState([])
+  const [isLoading, setIsLoading] = React.useState(true)
+  const query = "interstellar"
+
+  React.useEffect(() => {
+    fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`)
+    .then(response => response.json())
+    .then(data => {
+      setMovies(data.Search)
+      setIsLoading(false)
+    })
+  }, [])
+
   return (
     <div>
       <h1>Pop Corn App</h1>
+      {isLoading ? <h3>Loading ...</h3> : <h3>{movies[0].Title}</h3>}
     </div>
   );
 }
